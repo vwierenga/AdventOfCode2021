a67713906e4f6b69d6320c3c1317caaea9bc4e0d
Vincent <vincent@rometron.nl>
2021-12-01 11:33:01 +0100

    First day code

---
+++ Day1A/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace AdventOfCode2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = System.IO.File.ReadAllLines(@"input.txt");
+            int lastNumber = 0;
+            int increases = -1; //hacks
+
+            System.Console.WriteLine("Scanning depth...");
+            foreach (string line in lines)
+            {
+                int number = Int16.Parse(line);
+
+                if (number > lastNumber)
+                {
+                    increases++;
+                    //System.Console.WriteLine("Depth increased from " + lastNumber + " to " + number);
+                }
+                    
+
+                lastNumber = number;
+            }
+
+            System.Console.WriteLine("Number of depth increases: " + increases);
+        }
+    }
+}
---
+++ Day1B/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day1B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = System.IO.File.ReadAllLines(@"input.txt");
+            int increases = 0;
+
+            List<int> window = new List<int>();
+
+            System.Console.WriteLine("Scanning depth using a sliding window...");
+            foreach (string line in lines)
+            {
+                int number = Int16.Parse(line);
+
+                window.Add(number);
+
+                if (window.Count >= 4)
+                {
+                    int prefWindow = window[0] + window[1] + window[2];
+                    int thisWindow = window[1] + window[2] + window[3];
+
+                    if (prefWindow < thisWindow)
+                        increases++;
+
+                    window.RemoveAt(0);
+                }
+            }
+            System.Console.WriteLine("Number of sliding window increases: " + increases);
+        }
+    }
+}
