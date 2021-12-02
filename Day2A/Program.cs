d3c5603e40735133da67cb39fce7c3c4af769220
Vincent <vincent@rometron.nl>
2021-12-02 08:47:07 +0100

    Added the solutions for day 2

---
+++ Day2A/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Day2A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int depth = 0;
+            int horizontal = 0;
+
+            string[] lines = System.IO.File.ReadAllLines(@"input.txt");
+
+            foreach (string line in lines)
+            {
+                string[] command = line.Split(' ');
+
+                switch (command[0])
+                {
+                    case "up":
+                        depth = depth - Int16.Parse(command[1]);
+                        break;
+
+                    case "down":
+                        depth = depth + Int16.Parse(command[1]);
+                        break;
+
+                    case "forward":
+                        horizontal = horizontal + Int16.Parse(command[1]);
+                        break;
+
+                    default:
+                        System.Console.WriteLine("Unexpected command received!");
+                        break;
+                }
+            }
+
+            System.Console.WriteLine("Output: " + horizontal * depth);
+        }
+    }
+}
---
+++ Day2B/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Day2B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int aim = 0;
+            int depth = 0;
+            int horizontal = 0;
+
+            string[] lines = System.IO.File.ReadAllLines(@"input.txt");
+
+            foreach (string line in lines)
+            {
+                string[] command = line.Split(' ');
+
+                switch (command[0])
+                {
+                    case "up":
+                        aim = aim - Int16.Parse(command[1]);
+                        break;
+
+                    case "down":
+                        aim = aim + Int16.Parse(command[1]);
+                        break;
+
+                    case "forward":
+                        int x = Int16.Parse(command[1]);
+                        horizontal = horizontal + x;
+                        depth = depth + aim * x;
+                        break;
+
+                    default:
+                        System.Console.WriteLine("Unexpected command received!");
+                        break;
+                }
+            }
+
+            System.Console.WriteLine("Output: " + horizontal * depth);
+        }
+    }
+}
